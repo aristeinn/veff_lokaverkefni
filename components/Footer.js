70dc6cf9b69a9b28a1354d61ac057c1f91fcadc9
aristeinn <aristeinn@gmail.com>
2016-11-10 22:59:10 +0000

    er að vinna í footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,34 @@
 import React from 'react';
 
 var Footer = React.createClass({
-  render: function() {
-    var footerStyle = {
-      border: 'solid black'
+  render: function () {
+    var FooterStyle = {
+      backgroundColor: "#E4E4E4",
+      position: "absolute",
+      right: 0,
+      bottom: 0,
+      left: 0,
     };
-      return(
-          <div style={footerStyle}>
-            <h2>Ari Steinn Gudmundsson</h2>
-          </div>
-        );
+    var CenterStyle = {
+      textAlign: "center",
+      fontSize: 20,
+    };
+    var RightStyle = {
+      textAlign: "right",
+      fontSize: 20,
+    };
+    var LeftStyle = {
+      textAlign: "right",
+      fontSize: 20,
+      height: 20
+    };
+    return (
+      <div style={FooterStyle}>
+        <h5 style={CenterStyle}>Ari Steinn Guðmundsson</h5>
+        <h5 style={RightStyle}>Vefsíðuhönnun 303 2016</h5>
+        <h5><a href="https://github.com/aristeinn" style={LeftStyle}>https://github.com/aristeinn</a></h5>
+      </div>
+    );
   }
 });
 export default Footer;
